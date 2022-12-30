2a55f0477fd4318df22453c0ba755e72c6b5088f
cshjdr <cristian.rios@fit.com.ar>
2022-12-30 10:49:06 -0300

    cambio de texto del h1 en el header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header className='header'>
             <div className="header__contenedor">
-                <h1>Anota tus tareas y <span>cumplelas 😁!</span></h1>
+                <h1>Organiza tus tareas <span>y pendientes 😁!</span></h1>
                 <div class="scroll-down">
                     <a href="#crear" class="mouse-wrapper">
                         <span class="mouse">
